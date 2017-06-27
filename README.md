8cfe9fe5d44fdb5c53dfeeb38ba84af225b1432f
Hexalyse <reversebob@gmail.com>
2017-06-27 15:43:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# LightningTweeter
+
+This script uses the AS9535 sensor to detect lightning strikes and send tweets with related information : lightning energy, distance to the storm.
+
+## Dependencies
+
+This script uses those libraries :
+
+- [RaspberryPi-AS3935](https://github.com/pcfens/RaspberryPi-AS3935/) : `pip install RPi_AS3935`
+- [Tweepy](http://www.tweepy.org/) : `pip install tweepy`
+
+You also need to configure I2C on your Raspberry Pi. Adafruit made a [tutorial](https://learn.adafruit.com/adafruits-raspberry-pi-lesson-4-gpio-setup/configuring-i2c) explaining how to.
+
+If you want to know more about how I use it, you can read [this article](https://hexaly.se/2017/06/23/lightning-strike…ets-storm-alerts/).
---
+++ lightning_tweeter.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from RPi_AS3935 import RPi_AS3935
+import RPi.GPIO as GPIO
+import thread
+import time
+from datetime import datetime
+import tweepy
+
+# Initialize Twitter API
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+# Initialize GPIO
+GPIO.setmode(GPIO.BCM)
+
+# pin used for interrupts
+pin = 17
+# Rev. 1 Raspberry Pis should leave bus set at 0, while rev. 2 Pis should set
+# bus equal to 1. The address should be changed to match the address of the
+# sensor.
+sensor = RPi_AS3935(address=0x03, bus=1)
+# Indoors = more sensitive (can miss very strong lightnings)
+# Outdoors = less sensitive (can miss far away lightnings)
+sensor.set_indoors(True)
+sensor.set_noise_floor(0)
+# Change this value to the tuning value for your sensor
+sensor.calibrate(tun_cap=0x01)
+
+last_alert = datetime.min
+strikes_since_last_alert = 0
+
+
+# We use a function to send tweet so that we can run it in a different thread and avoid spending too much time in the
+# interrupt handle
+def send_tweet(tweet):
+    api.update_status(tweet)
+
+
+# Interrupt handler
+def handle_interrupt(channel):
+    global last_alert
+    global strikes_since_last_alert
+    global sensor
+    current_timestamp = datetime.now()
+    time.sleep(0.003)
+    reason = sensor.get_interrupt()
+    if reason == 0x01:
+        print("Noise level too high - adjusting")
+        sensor.raise_noise_floor()
+    elif reason == 0x04:
+        print("Disturber detected. Masking subsequent disturbers")
+        sensor.set_mask_disturber(True)
+    elif reason == 0x08:
+        print("We sensed lightning! (%s)" % current_timestamp.strftime('%H:%M:%S - %Y/%m/%d'))
+        if (current_timestamp - last_alert).seconds < 300:
+            print("Last strike is too recent, incrementing counter since last alert.")
+            strikes_since_last_alert += 1
+            return
+        last_alert = current_timestamp
+        distance = sensor.get_distance()
+        energy = sensor.get_energy()
+        print("Energy: " + str(energy) + " - distance: " + str(distance) + "km")
+        # Yes, it tweets in French. Baguette.
+        if strikes_since_last_alert == 0:
+            thread.start_new_thread(send_tweet, (
+                "Eclair détecté ! Puissance : {0} - distance du front de tempête : {1}km".format(energy, distance),))
+        else:
+            thread.start_new_thread(send_tweet, (
+                "/!\ {2} éclairs détectés ces 5 dernières minutes. Puissance dernier éclair : {0} - distance du front de tempête : {1}km".format(
+                    energy, distance, strikes_since_last_alert + 1),))
+    strikes_since_last_alert = 0
+
+
+# Use a software Pull-Down on interrupt pin
+GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+sensor.set_mask_disturber(False)
+GPIO.add_event_detect(pin, GPIO.RISING, callback=handle_interrupt)
+
+print("Waiting for lightning - or at least something that looks like it")
+
+try:
+    while True:
+        # Read/clear the sensor data every 10s in case we missed an interrupt (interrupts happening too fast ?)
+        time.sleep(10)
+        handle_interrupt(pin)
+finally:
+    # cleanup used pins... just because we like cleaning up after us
+    GPIO.cleanup()
